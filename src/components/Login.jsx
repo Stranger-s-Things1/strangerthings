1ec099978c755f26a2233fff700d48ba79d95c10
numelb <numelb@gmail.com>
2023-08-12 16:16:37 -0500

    edited LoginForm, added Register component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,35 @@
  
+    import React,{useState} from "react";
+    
+
+    
     const  LoginForm=() =>{
+        const [username, setUsername] = useState('')
+        const [password, setPassword]= useState('')
+
+        const handleSubmit =(e)=>{
+            e.preventDefault()
+            console.log(username)
+
+        }
         return (
             <>
              <form>
              <h1>LOG IN</h1>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input value={username} input type="text" placeholder="username" id="username"/>
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input value={password} input type="password" id="password"/>
                 </label>
                 <div>
                     <button type="submit">LOG IN</button>
                 </div>
+               
             </form>
+            <button>Don't have an account? Register here</button>
             </>
         )
     }
