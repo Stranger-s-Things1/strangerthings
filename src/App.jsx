74a5aa3719797cdff53d3e3d616e0398cef43bc6
Chrisford24 <Christopher11ford@yahoo.com>
2023-08-14 11:08:56 -0500

    additional push up

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { createContext, useState } from 'react'
 import Posts from './components/Posts'
 import PostForm from './components/PostForm'
 import Register from './components/Register'
 import Home from './Pages/Home'
 import Profile from './Pages/Profile'
-
-
 import './App.css'
 
-function App() {
+export const LoginContext = createContext();
 
+function App() {
+  const [loggedIn, setLoggedIn] = useState(true);
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/profile" element={<Profile />}/>
-      <Route path="/users/register" element={<Register />}/>
-      <Route path="/posts" element={<Posts />}/>
-      <Route path="/post/form" element={<PostForm />} />
-    </Routes>
+      <LoginContext.Provider value={loggedIn}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/users/register" element={<Register />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/post/form" element={<PostForm />} />
+          </Routes>
+        </BrowserRouter>
+      </LoginContext.Provider>
 
     </>
   )
