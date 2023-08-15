050ffb35473d682945f6bd5cbdb21ba880a11922
Chrisford24 <Christopher11ford@yahoo.com>
2023-08-14 21:43:53 -0500

    Post from submits but doesn't clear will fix later

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,19 +1,37 @@
 import { POST_API_URL } from "../api/api"
 import NavBar from "./NavBar"
-const handleSubmit = async (e) =>{
-     
 
-    // try{
-    //     const response = await fetch(`${POST_API_URL}`, {
-    //         method: form.method,
-    //         headers: {
-    //             'Content-Type':'application/json'
-    //         },
-            
-    //     })
-    // } catch(error){
-    //     console.log(error);
-    // }
+
+const handleSubmit = async (e) =>{  
+    
+    e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const postData = Object.fromEntries(formData.entries());
+
+    try{
+        const response = await fetch(`${POST_API_URL}`, {
+            method: form.method,
+            headers: {
+                'Content-Type':'application/json',
+                Authorization : `Bearer ${localStorage.getItem("Token")}`
+            },
+            body: JSON.stringify({
+                post: {
+                    "title": `${postData.title}`,
+                    "description": `${postData.description}`,
+                    "price": `${postData.price}`,
+                    "location": `${postData.location}`,
+                    "willDeliver": `${postData.willDeliver}`,
+                    
+                }
+            })            
+        })
+        const result = await response.json();
+        console.log(result)
+    } catch(error){
+        console.log(error);
+    }
 }
 
 const PostForm = () =>{
