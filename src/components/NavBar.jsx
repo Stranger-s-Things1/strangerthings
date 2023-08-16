be2c7ab75d05218a771fd417e8f5011db864025d
dmcoomer1985 <dmcoomer1985@gmail.com>
2023-08-15 20:41:10 -0400

    resolved merge conflicts

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,17 +10,11 @@ const NavBar = () => {
             </div>
             <div className= "navbar">
                 {/* <Link>HOME</Link> */}
-<<<<<<< HEAD
-                <Link className= "link" to="/posts">POSTS</Link>
-                <Link className= "link" to="/profile">PROFILE</Link>
-                <Link className= "link">LOG OUT</Link>
-=======
-                <Link to="/posts">POSTS</Link>
-                <Link to="/profile">{localStorage.Token ? "PROFILE" : null}</Link>
-                <Link to="/"onClick={()=>{
+                <Link className="link" to="/posts">POSTS</Link>
+                <Link className="link" to="/profile">{localStorage.Token ? "PROFILE" : null}</Link>
+                <Link className="link" to="/"onClick={()=>{
                     localStorage.clear();
                 }}>{localStorage.Token ? "LOG OUT" : "LOG IN"}</Link>
->>>>>>> 03520a3e2386723fcfe13528a373b8ae64cd6bda
             </div>
         </nav>
     )
